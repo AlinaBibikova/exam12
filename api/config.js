b2dcdabbfe3aaeef447cc7b5ff9af0f53d8cfbfc
Alina Bibikova <alinochkakleopatra@gmail.com>
2019-05-24 11:54:55 +0600

    #1 добавила users

---
+++ api/config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+const rootPath = __dirname;
+
+module.exports = {
+    rootPath,
+    uploadPath: path.join(rootPath, 'public/uploads'),
+    dbUrl: 'mongodb://localhost/exam12',
+    mongoOptions: {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+    },
+    facebook: {
+        appId: '545138079223618',
+        appSecret: '739a3bb638b68d5d77acd655a8248dbd'
+    }
+};
