06995781545323832a19504cc6e573ff6d66a72b
Alina Bibikova <alinochkakleopatra@gmail.com>
2019-05-25 18:10:21 +0600

    #2 добавила просмотр картинки

--- frontend/src/containers/Gallery/Gallery.js
+++ frontend/src/containers/Gallery/Gallery.js
@@ -3,6 +3,7 @@ import {connect} from "react-redux";
 import {deletePhoto, fetchGalleries, fetchPhoto} from "../../store/actions/galleriesActions";
 import Loader from "../../components/UI/Loader/Loader";
 import {apiURL} from "../../constants";
+import {Button, Card, CardFooter} from "reactstrap";
 
 class Gallery extends Component {
     async componentDidMount()  {
@@ -13,33 +14,35 @@ class Gallery extends Component {
     render() {
 
         const photo = this.props.photo;
-        const userDisplayName = this.props.galleries && this.props.galleries.user && this.props.galleries.user.displayName;
         const userId = this.props.user && this.props.user._id && this.props.user.displayName;
 
+        console.log(photo);
         return (
 
             <div>
                 {this.props.loading && <Loader/>}
 
-                {userId || userDisplayName ?
-                    <h3 className="mb-3">{userId} gallery</h3>
-                    : null}
+                <Card>
+                    {userId && photo ?
+                        <h3 className="mb-3">{userId} gallery</h3>
+                        : null}
 
-                <p>
-                    <strong>
-                        {photo.title}
-                    </strong>
-                </p>
-                {photo.image && (
-                    <img src={`${apiURL}/uploads/${photo.image}`} className="item-img" alt={photo.title}/>
-                )}
+                    <p>
+                        <strong>
+                            {photo.title}
+                        </strong>
+                    </p>
+                    {photo.image && (
+                        <img src={`${apiURL}/uploads/${photo.image}`} className="item-img" alt={photo.title}/>
+                    )}
 
-                {/*{userId === this.props.galleries.user._id*/}
-                    {/*? <CardFooter className="d-flex justify-content-between">*/}
-                        {/*<Button size="sm" color="danger" onClick={() => this.deletePhoto(this.props.match.params.id)}>Delete</Button>*/}
-                    {/*</CardFooter>*/}
-                    {/*: null*/}
-                {/*}*/}
+                    {userId
+                        ? <CardFooter className="d-flex justify-content-between">
+                            <Button size="sm" color="danger" onClick={() => this.deletePhoto(this.props.match.params.id)}>Delete</Button>
+                        </CardFooter>
+                        : null
+                    }
+                </Card>
             </div>
         );
     }
@@ -47,7 +50,6 @@ class Gallery extends Component {
 
 const mapStateToProps = state => ({
     photo: state.galleries.photo,
-    galleries: state.galleries.galleries,
     user: state.users.user,
     error: state.galleries.error,
     loading: state.galleries.loading
